5a99fb9c94ac7d5a073eaaed1def6c2c97c131ae
darijavan <darijavan@gmail.com>
2020-03-21 19:49:00 +0300

    add theme option to constructor

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,12 @@ class Clock {
   /**
    * Instantiate a new clock
    * @param {HTMLDivElement} el The div element container for the clock
-   * @param {{ smooth: boolean, size: number }} conf Configuration for the clock
+   * @param {{ smooth: boolean, size: number, theme: string }} conf Configuration for the clock
    */
   constructor(el, conf = {
     smooth: false,
-    size: 300
+    size: 300,
+    theme: 'light'
   }) {
     this.center = document.createElement('div');
     this.ph = document.createElement('div');
@@ -27,7 +28,7 @@ class Clock {
       sec = date.getSeconds(),
       ms = date.getMilliseconds(),
       sAngle = (sec + (this.conf.smooth ? ms / 1000 : 0) - 15) * Math.PI / 30,
-      mAngle = (min - 15 + (this.conf.smooth ? sec / 60 : 0)) * Math.PI / 30,
+      mAngle = (min - 15 + sec / 60) * Math.PI / 30,
       hAngle = (hour - 3 + min / 60) * Math.PI / 6;
 
     this.ph.style.transform = `rotateZ(${hAngle}rad)`;
@@ -42,6 +43,8 @@ class Clock {
    * @param {HTMLDivElement} clock The clock DOM element
    */
   initClock(clock) {
+    clock.classList.add(this.conf.theme);
+
     clock.style.width = `${this.conf.size ? this.conf.size : 300}px`;
     clock.style.height = `${this.conf.size ? this.conf.size : 300}px`;
 
@@ -72,11 +75,11 @@ class Clock {
       d.classList.add('big-dash');
 
       let angle = (i + 3) * Math.PI / 6,
-        dx = (dashContainer.offsetWidth / 2) * (1 - Math.cos(angle)),
-        dy = (dashContainer.offsetHeight / 2) * (1 - Math.sin(angle));
+        dx = 50 * (1 - Math.cos(angle)),
+        dy = 50 * (1 - Math.sin(angle));
 
-      d.style.left = `${dx}px`;
-      d.style.top = `${dy}px`;
+      d.style.left = `${dx}%`;
+      d.style.top = `${dy - 0.5}%`;
       d.style.transform = `rotateZ(${angle}rad)`;
       dashContainer.appendChild(d);
     }
@@ -88,11 +91,11 @@ class Clock {
         d.classList.add('small-dash');
 
         let angle = (i + 15) * Math.PI / 30,
-          dx = (dashContainer.offsetWidth / 2) * (1 - Math.cos(angle)),
-          dy = (dashContainer.offsetHeight / 2) * (1 - Math.sin(angle));
+          dx = 50 * (1 - Math.cos(angle)),
+          dy = 50 * (1 - Math.sin(angle));
 
-        d.style.left = `${dx}px`;
-        d.style.top = `${dy}px`;
+        d.style.left = `${dx}%`;
+        d.style.top = `${dy - 0.3}%`;
         d.style.transform = `rotateZ(${angle}rad)`;
         dashContainer.appendChild(d);
       }
